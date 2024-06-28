de0b5801ef90bc15cd67f200f33eafe5d4be2bac
Jon Weisberg <jonathan.weisberg@otthydromet.com>
2024-06-28 11:06:47 -0400

    Made some simple changes so documentation builds correctly

--- src/log_task_trigger.py
+++ src/log_task_trigger.py
@@ -18,6 +18,7 @@ def log_task_trigger():
     The label in the log will be script number and the trigger type with a value of 1.0.
         * Example 1: S2 Schedule
         * Example 2: S3 Boot
+        
     Multiple scripts can be setup with the same task but with different triggers.
     """
     _log_task_trigger("S" + str(index()) + " " + command_line("!S" + str(index()) + " trigger").strip())
